631fdb50312db84aa7fddb69a7832a09c33614bf
Сергей <bakay.sergey28@gmail.com>
2021-08-27 10:18:43 +0300

    first download to github

---
+++ app/js/scripts.min.js
@@ -0,0 +1,94 @@
+"use strict";
+
+function setActiveMenuItem() {
+	var current = location.pathname.split('/')[1];
+	if (current === "") return;
+	var menuItems = document.querySelectorAll('.menu__list a');
+	for (var i = 0, len = menuItems.length; i < len; i++) {
+		if (menuItems[i].getAttribute("href").indexOf(current) !== -1) {
+			menuItems[i].closest('.menu__item').children[0].className += " active";
+		}
+	}
+};
+
+document.addEventListener('DOMContentLoaded', function(){
+	setActiveMenuItem();
+
+	//big menu
+	const menu = document.getElementById("dilator");
+	const menuTrigger = document.querySelector(".menu__trigger");
+
+	 menuTrigger.addEventListener("click", function() {
+		menu.classList.toggle("open");
+		// this.classList.toggle("clicked");
+	 });
+
+	//all dialogs
+	 window.addEventListener('click', function(e) {
+		if (e.target.classList.contains('submenu__trigger')) {
+			e.preventDefault();
+			e.target.nextElementSibling.classList.toggle("shown");
+		}
+		else if (!e.target.closest('.shown')){
+			const dialogs = document.querySelectorAll(".dialog");
+			Array.from(dialogs).forEach(item => {
+				item.classList.remove('shown');
+			});
+		};
+
+		if (e.target.classList.contains('dialog__link')) {
+			e.target.closest(".dialog").classList.remove("shown");
+		};
+		if (e.target.classList.contains('btn--close')) {
+			e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+		};
+	 });
+
+	 //sorting (grid | lines)
+	const sorting = document.querySelector('.sort__grid');
+
+	if (sorting) {
+		sorting.addEventListener('click', function(e) {
+			const items = document.querySelectorAll('.btn');
+			const target = e.target;
+			[...items].forEach(item => {
+				item.classList.remove('active');
+			})
+			target.classList.toggle('active');
+
+			if (target.classList.contains('grid-trigger')) {
+				target.parentNode.parentNode.parentNode.parentNode.parentNode.classList.add("board--grid");
+			} else {
+				target.parentNode.parentNode.parentNode.parentNode.parentNode.classList.remove("board--grid");
+			}
+		});
+	};
+
+
+	// checkboxes
+	const checkboxMain = document.getElementById('checkbox-general');
+	const checkboxes = document.querySelectorAll('.checkbox');
+
+	if (checkboxMain) {
+		checkboxMain.addEventListener('change', function(e) {
+			for (let i=0; i < checkboxes.length; i++){
+				if (e.target.checked) {
+					checkboxes[i].checked = true;
+				} else {
+					checkboxes[i].checked = false;
+				}
+			}
+		});
+	};
+
+	const form = document.querySelector('.sort');
+	if (form) {
+		form.addEventListener('change', function(e) {
+			if (Array.from(checkboxes).every(elem => elem.checked)) {
+				checkboxMain.checked = true;
+			} else {checkboxMain.checked = false;}
+		});
+	};
+
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+var   gulp          = require('gulp'),
+		// gutil         = require('gulp-util' ),
+		sourcemaps 	  = require('gulp-sourcemaps'),
+		sass          = require('gulp-sass'),
+		browserSync   = require('browser-sync'),
+		concat        = require('gulp-concat'),
+		uglify        = require('gulp-uglify'),
+		cleancss      = require('gulp-clean-css'),
+		rename        = require('gulp-rename'),
+		// autoprefixer  = require('gulp-autoprefixer'),
+		notify        = require('gulp-notify'),
+		rigger        = require('gulp-rigger'),
+		// rsync         = require('gulp-rsync'),
+		imagemin      = require('gulp-imagemin');
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {baseDir: 'app'},
+		notify: false,
+		// open: false,
+		// online: false, // Work Offline Without Internet Connection
+		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+	})
+});
+
+
+gulp.task('layout', function () {
+   return gulp.src('src/*.html')
+   // .pipe(plumber())
+   .pipe(rigger())
+   .pipe(gulp.dest('app/'))
+   .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('styles', function() {
+	return gulp.src('src/scss/**/*.scss')
+	// return gulp.src('src/scss/*.scss')
+	.pipe(sourcemaps.init({loadMaps: true}))  //sourceMap
+	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+	// .pipe(autoprefixer(['last 4 versions']))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 }}})) // Opt., comment out when debugging
+	.pipe(gulp.dest('app/css'))
+	.pipe(sourcemaps.write('./'))
+	.pipe(gulp.dest('./app/css/'))
+	.pipe(browserSync.reload({ stream: true }));
+});
+// gulp.task('styles-separated', function() {
+// 	return gulp.src('src/scss/pages/*.scss')
+// 	.pipe(sourcemaps.init({loadMaps: true}))
+// 	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+// 	.pipe(rename({ suffix: '.min', prefix : '' }))
+// 	.pipe(cleancss( {level: { 1: { specialComments: 0 }}})) // Opt., comment out when debugging
+// 	.pipe(gulp.dest('app/css'))
+// 	.pipe(sourcemaps.write('./'))
+// 	.pipe(gulp.dest('./app/css/'))
+// 	.pipe(browserSync.reload({ stream: true }));
+// });
+
+gulp.task('scripts', function() {
+	return gulp.src([
+		'src/js/main.js', // Always (scripts) at the end
+		])
+	.pipe(concat('scripts.min.js'))
+	// .pipe(uglify()) // Minify js - opt.
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('code', function() {
+	return gulp.src('app/*.html')
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+
+gulp.task('watch', function() {
+	gulp.watch('src/scss/**/*.scss', gulp.parallel('styles'));
+	// gulp.watch('src/scss/**/*.scss', gulp.parallel('styles-separated'));
+	gulp.watch(['libs/**/*.js', 'src/js/main.js'], gulp.parallel('scripts'));
+	gulp.watch('src/**/*.html', gulp.parallel('layout'))
+});
+gulp.task('default', gulp.parallel('layout','styles', 'scripts', 'browser-sync', 'watch'));
