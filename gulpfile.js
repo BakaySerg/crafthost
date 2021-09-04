85928c9be1386e9e2962365e24461922923797e4
Сергей <bakay.sergey28@gmail.com>
2021-09-04 13:37:11 +0300

    form-input script ready

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('browser-sync', function() {
 		server: {baseDir: 'app'},
 		notify: false,
 		// open: false,
-		// online: false, // Work Offline Without Internet Connection
+		online: false, // Work Offline Without Internet Connection
 		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
 	})
 });
