afd879ba418cce333c0efa588fae0b835dbe5325
bakay_serg <bakay.sergey28@gmail.com>
2023-12-26 22:26:53 +0200

    dedicated-item ready

--- src/js/main.js
+++ src/js/main.js
@@ -26,86 +26,83 @@ document.addEventListener('DOMContentLoaded', function(){
 	const menu = document.getElementById("dilator");
 	const menuTrigger = document.querySelector(".menu__trigger");
 
-	 menuTrigger?.addEventListener("click", function() {
+	menuTrigger?.addEventListener("click", function () {
 		menu.classList.toggle("open");
 		menuTrigger.classList.toggle("menu__trigger--cross");
-	 });
+	});
 
 	//all dialogs
-	 window.addEventListener('click', function(e) {
-		if (e.target.classList.contains('submenu__trigger')) {
+	window.addEventListener("click", function (e) {
+		if (e.target.classList.contains("submenu__trigger")) {
 			e.preventDefault();
-			const allDialogs = document.querySelectorAll('.shown');
+			const allDialogs = document.querySelectorAll(".shown");
 			const needDialog = e.target.nextElementSibling;
-			const showns = [...allDialogs].filter(item => item !== needDialog);
-			[...showns].forEach(item => {
-				item.classList.remove('shown');
+			const showns = [...allDialogs].filter((item) => item !== needDialog);
+			[...showns].forEach((item) => {
+				item.classList.remove("shown");
 			});
-			needDialog.classList.toggle("shown")
+			needDialog.classList.toggle("shown");
+			window.navigator.vibrate(30);
+		} else if (!e.target.closest(".shown")) {
+			const dialogs = document.querySelectorAll(".dialog");
+			Array.from(dialogs).forEach((item) => {
+				item.classList.remove("shown");
+			});
+		}
+
+		if (e.target.classList.contains("dialog__link")) {
+			e.target.closest(".dialog").classList.remove("shown");
+		}
+		if (e.target.classList.contains("btn--close")) {
+			e.target.closest(".board__message").remove();
 			window.navigator.vibrate(30);
 		}
-		else if (!e.target.closest('.shown')){
-			const dialogs = document.querySelectorAll(".dialog");
-			Array.from(dialogs).forEach(item => {
-				item.classList.remove('shown');
-			});
-		};
-
-		if (e.target.classList.contains('dialog__link')) {
-			e.target.closest(".dialog").classList.remove("shown");
-		};
-		if (e.target.classList.contains('btn--close')) {
-			e.target.closest('.board__message').remove();
-			window.navigator.vibrate(30);
-		};
-	 });
+	});
 
 	/**
 		 sorting (grid | lines)
 	**/
-	const sorting = document.querySelector('.sort__grid');
+	const sorting = document.querySelector(".sort__grid");
 
 	if (sorting) {
-		sorting.addEventListener('click', function(e) {
-			const items = sorting.querySelectorAll('.btn');
+		sorting.addEventListener("click", function (e) {
+			const items = sorting.querySelectorAll(".btn");
 			const target = e.target;
-			[...items].forEach(item => {
-				item.classList.remove('active');
-			})
-			target.classList.toggle('active');
+			[...items].forEach((item) => {
+				item.classList.remove("active");
+			});
+			target.classList.toggle("active");
 
-			if (target.classList.contains('grid-trigger')) {
-				target.closest('.board--lg').classList.add("board--grid");
+			if (target.classList.contains("grid-trigger")) {
+				target.closest(".board--lg").classList.add("board--grid");
 			} else {
-				target.closest('.board--lg').classList.remove("board--grid");
+				target.closest(".board--lg").classList.remove("board--grid");
 			}
 		});
-	};
+	}
 
 	/**
 		 temp link
 	**/
 	const tempLink = function () {
-		[].forEach.call(
-			document.querySelectorAll("[data-link]"),
-			function (el) {
-				el.addEventListener("click", function (e) {
-					e.preventDefault();
-					window.location.href = this.getAttribute('data-link');
-				});
+		[].forEach.call(document.querySelectorAll("[data-link]"), function (el) {
+			el.addEventListener("click", function (e) {
+				e.preventDefault();
+				window.location.href = this.getAttribute("data-link");
 			});
-		};
+		});
+	};
 	tempLink();
 
 	/**
 		 checkboxes
 	**/
-	const checkboxMain = document.getElementById('checkbox-general');
-	const checkboxes = document.querySelectorAll('.checkbox');
+	const checkboxMain = document.getElementById("checkbox-general");
+	const checkboxes = document.querySelectorAll(".checkbox");
 
 	if (checkboxMain) {
-		checkboxMain.addEventListener('change', function(e) {
-			for (let i=0; i < checkboxes.length; i++){
+		checkboxMain.addEventListener("change", function (e) {
+			for (let i = 0; i < checkboxes.length; i++) {
 				if (e.target.checked) {
 					checkboxes[i].checked = true;
 				} else {
@@ -113,50 +110,49 @@ document.addEventListener('DOMContentLoaded', function(){
 				}
 			}
 		});
-	};
+	}
 
-	const form = document.querySelector('.sort');
+	const form = document.querySelector(".sort");
 	if (form) {
-		form.addEventListener('change', function(e) {
-			if (Array.from(checkboxes).every(elem => elem.checked)) {
+		form.addEventListener("change", function (e) {
+			if (Array.from(checkboxes).every((elem) => elem.checked)) {
 				checkboxMain.checked = true;
-			} else {checkboxMain.checked = false;}
+			} else {
+				checkboxMain.checked = false;
+			}
 		});
-	};
+	}
 
 	/**
 		 Search fraction (temp solution)
 	**/
-	const search = document.getElementById('searchBtn');
+	const search = document.getElementById("searchBtn");
 	if (search) {
-		search.addEventListener('click', function(e) {
+		search.addEventListener("click", function (e) {
 			const searchField = e.target.previousElementSibling;
-			const planDetails = document.querySelector('.plan__sidelong');
-			const argums = document.querySelector('.plan__arguments');
+			const planDetails = document.querySelector(".plan__sidelong");
+			const argums = document.querySelector(".plan__arguments");
 			if (searchField.value.length >= 4) {
-				planDetails.removeAttribute('hidden');
-				argums.setAttribute('hidden','');
+				planDetails.removeAttribute("hidden");
+				argums.setAttribute("hidden", "");
 				e.preventDefault();
 			}
 		});
-	};
+	}
 
 	const showAddWindow = document.querySelectorAll("[data-show]");
 	if (showAddWindow) {
-		[].forEach.call(
-			showAddWindow, function (el) {
-				el.addEventListener("click", function (e) {
-					e.preventDefault();
-					const parentBox = this.parentElement;
-					let id = this.getAttribute("data-show");
-					let comingContent = document.getElementById(id);
-					parentBox.setAttribute('hidden','');
-					comingContent.removeAttribute('hidden');
-				});
-			}
-		);
-	};
-
+		[].forEach.call(showAddWindow, function (el) {
+			el.addEventListener("click", function (e) {
+				e.preventDefault();
+				const parentBox = this.parentElement;
+				let id = this.getAttribute("data-show");
+				let comingContent = document.getElementById(id);
+				parentBox.setAttribute("hidden", "");
+				comingContent.removeAttribute("hidden");
+			});
+		});
+	}
 
 	/**
 		tabs inside the drawer (modal)
@@ -167,10 +163,10 @@ document.addEventListener('DOMContentLoaded', function(){
 			function (el) {
 				el.addEventListener("click", function (e) {
 					let id = this.getAttribute("data-trigger-modal"),
-						 comingTab = document.getElementById(id),
-						 parent = comingTab.parentElement,
-						 siblingsTab = parent.children,
-						 currentTab = parent.querySelector('[data-tab="active"]');
+						comingTab = document.getElementById(id),
+						parent = comingTab.parentElement,
+						siblingsTab = parent.children,
+						currentTab = parent.querySelector('[data-tab="active"]');
 
 					[...siblingsTab].forEach((sibling) => {
 						if (sibling !== comingTab) {
@@ -179,20 +175,20 @@ document.addEventListener('DOMContentLoaded', function(){
 					});
 					comingTab.setAttribute("data-tab", "active");
 
-					if (this.getAttribute('type') === "reset" && currentTab) {
+					if (this.getAttribute("type") === "reset" && currentTab) {
 						let filled = currentTab.querySelectorAll(".form-field");
-						let btn = currentTab.querySelector('[type=submit]');
+						let btn = currentTab.querySelector("[type=submit]");
 						[...filled].forEach((f) => {
 							f.classList.remove("filled");
 						});
 						btn.classList.add("btn--disabled");
 
-						let fileInp = currentTab.querySelector('[type=file]');
+						let fileInp = currentTab.querySelector("[type=file]");
 						if (fileInp) {
 							let label = fileInp.nextElementSibling,
-								 span = label.querySelector('span'),
-								 defaultText = span.getAttribute('data-text');
-								 span.innerHTML = defaultText;
+								span = label.querySelector("span"),
+								defaultText = span.getAttribute("data-text");
+							span.innerHTML = defaultText;
 						}
 					}
 				});
@@ -210,22 +206,23 @@ document.addEventListener('DOMContentLoaded', function(){
 			function (el) {
 				el.addEventListener("click", function (e) {
 					let id = this.getAttribute("data-trigger-tab"),
-						 comingTab = document.getElementById(id),
-						 parent = comingTab.closest('.tabs-content'),
-						 currentTab = parent.querySelector('[data-tab="active"]');
+						comingTab = document.getElementById(id),
+						parent = comingTab.closest(".tabs-content"),
+						currentTab = parent.querySelector('[data-tab="active"]');
 
 					currentTab?.setAttribute("data-tab", "hidden");
 					comingTab.setAttribute("data-tab", "active");
-					if (this.classList.contains('selector--tab')){
-						let othersTabs = this.closest('.tabs').querySelectorAll('.selector');
-						[...othersTabs].forEach(item => {
-							item.classList.remove('checked');
+					if (this.classList.contains("selector--tab")) {
+						let othersTabs =
+							this.closest(".tabs").querySelectorAll(".selector");
+						[...othersTabs].forEach((item) => {
+							item.classList.remove("checked");
 						});
-						this.classList.add('checked');
+						this.classList.add("checked");
 					}
-					if (this.getAttribute('data-counter')) {
-						let step = document.querySelector('.step__current');
-						let val = this.getAttribute('data-counter');
+					if (this.getAttribute("data-counter")) {
+						let step = document.querySelector(".step__current");
+						let val = this.getAttribute("data-counter");
 						step.innerHTML = val;
 					}
 				});
@@ -272,6 +269,33 @@ document.addEventListener('DOMContentLoaded', function(){
 
 	expandersHandler();
 
+	// custom select
+	let selectCustom = function () {
+		let selectHeader = document.querySelectorAll(".select-cstm__header");
+		let selectItem = document.querySelectorAll(".select-cstm__item");
+
+		selectHeader.forEach((item) => {
+			item.addEventListener("click", selectToggle);
+		});
+		selectItem.forEach((item) => {
+			item.addEventListener("click", selectChoose);
+		});
+		function selectToggle() {
+			this.parentElement.classList.toggle("is-active");
+		}
+		function selectChoose() {
+			let text = this.innerHTML,
+				value = this.getAttribute("data-value"),
+				select = this.closest(".select-cstm"),
+				input = select.querySelector("input[type=hidden]"),
+				currentText = select.querySelector(".select-cstm__current");
+			currentText.innerHTML = text;
+			input.value = value;
+			select.classList.remove("is-active");
+		}
+	};
+	selectCustom();
+
 	/**
 		custom placeholder
 	**/
@@ -279,7 +303,7 @@ document.addEventListener('DOMContentLoaded', function(){
 		function getInputFields() {
 			let inputFields = document.getElementsByClassName("form-input");
 			return [...inputFields];
-		};
+		}
 
 		function checkLabel() {
 			let fieldParent = this.parentElement;
@@ -288,11 +312,11 @@ document.addEventListener('DOMContentLoaded', function(){
 			} else {
 				fieldParent.classList.remove("filled");
 			}
-		};
+		}
 
 		function focusLabel() {
 			this.parentElement.classList.add("filled");
-		};
+		}
 
 		getInputFields().forEach(function (el) {
 			el.addEventListener("focus", focusLabel);
@@ -300,21 +324,22 @@ document.addEventListener('DOMContentLoaded', function(){
 		});
 	})();
 
-	let formsChanged = document.querySelectorAll('.drawer__form');
-	if (formsChanged){
-		[...formsChanged].forEach(function(el){
-			el.addEventListener("keyup",function(){
-				let inputs = el.querySelectorAll('[required]');
-				let btn = el.querySelector('[type=submit]');
+	let formsChanged = document.querySelectorAll(".drawer__form");
+	if (formsChanged) {
+		[...formsChanged].forEach(function (el) {
+			el.addEventListener("keyup", function () {
+				let inputs = el.querySelectorAll("[required]");
+				let btn = el.querySelector("[type=submit]");
 
 				let allValid = true;
-				if (!el.classList.contains('pay')) {
+				if (!el.classList.contains("pay")) {
 					[...inputs].forEach((inp) => {
-						if (inp.value.length < 2){  // && inp.value.trim() !== ""
+						if (inp.value.length < 2) {
+							// && inp.value.trim() !== ""
 							allValid = false;
 						}
 					});
-					if (!allValid){
+					if (!allValid) {
 						btn.classList.add("btn--disabled");
 					} else {
 						btn.classList.remove("btn--disabled");
@@ -322,12 +347,11 @@ document.addEventListener('DOMContentLoaded', function(){
 				}
 			});
 		});
-	};
+	}
 
 	// copyright - year
 	const year = document.getElementById("year");
 	if (year) {
 		year.innerHTML = new Date().getFullYear();
-	};
-
+	}
 });
